aefa5abc9bf6e5f169e76045a1534536016338e9
junming403 <wjmfelix0403@gmail.com>
2019-09-28 16:53:51 +0800

    Add new order transaction

--- src/main/java/edu/cs4224/CqlQueryList.java
+++ src/main/java/edu/cs4224/CqlQueryList.java
@@ -1,6 +1,25 @@
 package edu.cs4224;
 
 public class CqlQueryList {
-  public static final String DISTRICT_NEXT_ORDER_ID
-      = "Select D_NEXT_O_ID from district where D_W_ID = %d and D_ID = %d";
+  public static final String DISTRICT_INFO
+          = "SELECT D_NEXT_O_ID, D_TAX FROM district WHERE D_W_ID = %d AND D_ID = %d";
+  public static final String INCREMENT_NEXT_ORDER_ID
+          = "UPDATE district SET D_NEXT_O_ID = D_NEXT_O_ID + 1 WHERE D_W_ID = %d AND D_ID = %d";
+  public static final String CREATE_NEW_ORDER
+          = "INSERT INTO customer_order (O_ID, O_D_ID, O_W_ID, O_C_ID, O_ENTRY_D, O_OL_CNT, O_ALL_LOCAL) " +
+          "VALUES('%d', '%d', '%d', '%d', '%s', '%d', '%d')";
+  public static final String CHECK_STOCK_QUANTITY
+          = "SELECT S_QUANTITY FROM stock WHERE S_W_ID = %d AND S_I_ID = %d";
+  public static final String UPDATE_STOCK
+          = "UPDATE stock SET S_QUANTITY=%d, S_YTD=S_YTD+%d, S_ORDER_CNT=S_ORDER_CNT+1, " +
+          "S_REMOTE_CNT=S_ORDER_CNT+%d WHERE S_W_ID = %d AND S_I_ID = %d";
+  public static final String CHECK_ITEM_INFO
+          = "SELECT I_PRICE, I_NAME FROM item WHERE I_ID = %d";
+  public static final String CREATE_ORDER_LINE
+          = "INSERT INTO order_line(OL_O_ID, OL_D_ID, OL_W_ID, OL_NUMBER, OL_I_ID, OL_SUPPLY_W_ID, OL_QUANTITY, OL_AMOUNT, OL_DIST_INFO)" +
+          " VALUES(%d, %d, %d, %d, %d, %d, %d, %f, %s)";
+  public static final String CHECK_WAREHOUSE_TAX
+          = "SELECT W_TAX FROM warehouse WHERE W_ID=%d";
+  public static final String CHECK_CUSTOMER_INFO
+          = "SELECT C_DISCOUNT, C_LAST, C_CREDIT FROM customer WHERE C_W_ID=%d, C_D_ID=%d, C_ID=%d";
 }
--- src/main/java/edu/cs4224/transactions/NewOrderTransaction.java
+++ src/main/java/edu/cs4224/transactions/NewOrderTransaction.java
@@ -5,7 +5,10 @@ import com.datastax.oss.driver.api.core.cql.Row;
 
 import edu.cs4224.CqlQueryList;
 
+import java.text.Format;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 /**
@@ -16,6 +19,8 @@ public class NewOrderTransaction extends BaseTransaction {
   private final int warehouseID;
   private final int districtID;
   private final int numDataLines;
+  private static final Format formatter = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss.SSS z");
+
 
   public NewOrderTransaction(final CqlSession session, final String[] parameters) {
     super(session, parameters);
@@ -31,23 +36,93 @@ public class NewOrderTransaction extends BaseTransaction {
   }
 
   @Override public void execute(final String[] dataLines) {
-    List<Integer> numsEach = new ArrayList<>();
+    List<Integer> itemIds = new ArrayList<>();
     List<Integer> supplierWarehouse = new ArrayList<>();
     List<Integer> quantity = new ArrayList<>();
 
     for (String dataLine: dataLines) {
       String[] parts = dataLine.split(",");
-      numsEach.add(Integer.parseInt(parts[0]));
+      itemIds.add(Integer.parseInt(parts[0]));
       supplierWarehouse.add(Integer.parseInt(parts[1]));
       quantity.add(Integer.parseInt(parts[2]));
     }
 
-    createNewOrder(numsEach, supplierWarehouse, quantity);
+    createNewOrder(itemIds, supplierWarehouse, quantity);
   }
 
-  private void createNewOrder(List<Integer> numsEach, List<Integer> supplierWareHouse, List<Integer> quantity) {
-    String query = String.format(CqlQueryList.DISTRICT_NEXT_ORDER_ID, warehouseID, districtID);
-    List<Row> rows = executeQuery(query);
-    System.out.println(rows.get(0).getInt("D_NEXT_O_ID"));
+  private void createNewOrder(List<Integer> itemIds, List<Integer> supplierWareHouse, List<Integer> quantity) {
+    String get_next_order_number_query = String.format(CqlQueryList.DISTRICT_INFO, warehouseID, districtID);
+    Row res = executeQuery(get_next_order_number_query).get(0);
+    int next_order_number = res.getInt("D_NEXT_O_ID");
+    double district_tax = res.getBigDecimal("D_TAX").doubleValue();
+    String increment_order_number_query =  String.format(CqlQueryList.INCREMENT_NEXT_ORDER_ID, warehouseID, districtID);
+    executeQuery(increment_order_number_query);
+
+    int isAllLocal = 1;
+    for (Integer integer : supplierWareHouse) {
+      if (integer != warehouseID) {
+        isAllLocal = 0;
+        break;
+      }
+    }
+
+    Date cur = new Date();
+    String order_time = formatter.format(cur);
+
+    String new_order_query = String.format(CqlQueryList.CREATE_NEW_ORDER, next_order_number, districtID,
+            warehouseID, customerID, order_time, numDataLines, isAllLocal);
+    executeQuery(new_order_query);
+
+    ArrayList<Integer> adjustedQuantities = new ArrayList<>();
+    ArrayList<Double> itemsAmount = new ArrayList<>();
+    ArrayList<String> itemsName = new ArrayList<>();
+
+    double totalAmount = 0;
+    for (int i = 0; i < numDataLines; i++) {
+        String check_stock_quantity_query = String.format(CqlQueryList.CHECK_STOCK_QUANTITY,
+                supplierWareHouse.get(i), itemIds.get(i));
+        int quantity_left = executeQuery(check_stock_quantity_query).get(0).getBigDecimal("S_QUANTITY").intValue();
+        int adjusted_quantity = quantity_left - quantity.get(i);
+        if (adjusted_quantity < 10) adjusted_quantity += 100;
+        adjustedQuantities.add(adjusted_quantity);
+        int remoteIncrement =  supplierWareHouse.get(i) == warehouseID ? 0 : 1;
+        String update_stock_query = String.format(CqlQueryList.UPDATE_STOCK, adjusted_quantity, quantity.get(i),
+                remoteIncrement, supplierWareHouse.get(i), itemIds.get(i));
+        executeQuery(update_stock_query);
+
+        String check_item_price_query = String.format(CqlQueryList.CHECK_ITEM_INFO, itemIds.get(i));
+        Row item_info = executeQuery(check_item_price_query).get(0);
+        double item_price = item_info.getDouble("I_PRICE");
+        String item_name = item_info.getString("I_NAME");
+        double itemAmount = quantity.get(i) * item_price;
+        itemsAmount.add(itemAmount);
+        itemsName.add(item_name);
+        totalAmount += itemAmount;
+        String create_order_line_query = String.format(CqlQueryList.CREATE_ORDER_LINE, next_order_number,
+                districtID, warehouseID, i + 1, itemIds.get(i), supplierWareHouse.get(i), quantity.get(i),itemAmount, "S_DIST_" + districtID);
+        executeQuery(create_order_line_query);
+    }
+    String check_warehouse_tax_query = String.format(CqlQueryList.CHECK_WAREHOUSE_TAX, warehouseID);
+    double warehouse_tax = executeQuery(check_warehouse_tax_query).get(0).getDouble("W_TAX");
+
+    String check_customer_discount_query = String.format(CqlQueryList.CHECK_CUSTOMER_INFO, warehouseID, districtID, customerID);
+    Row customer_info = executeQuery(check_customer_discount_query).get(0);
+    double user_discount = customer_info.getDouble("C_DISCOUNT");
+    String user_last_name = customer_info.getString("C_LAST");
+    String user_credit_status = customer_info.getString("C_CREDIT");
+
+
+    totalAmount = totalAmount * (1 + warehouse_tax + district_tax) * (1 - user_discount);
+
+    System.out.println("Transaction Summary:");
+    System.out.println(String.format("1. (W_ID: %d, D_ID: %d, C_ID, %d), C_LAST: %s, C_CREDIT: %s, C_DISCOUNT: %.4f",
+            warehouseID, districtID, customerID, user_last_name, user_credit_status, user_discount));
+    System.out.println(String.format("2. W_TAX: %.4f, D_TAX: %.4f", warehouse_tax, district_tax));
+    System.out.println(String.format("3. O_ID: %d, O_ENTRY_D: %s", next_order_number,  order_time));
+    System.out.println(String.format("4. NUM_ITEMS: %s, TOTAL_AMOUNT: %.2f", numDataLines, totalAmount));
+    for (int i = 0; i < numDataLines; i++) {
+      System.out.println(String.format("\t ITEM_NUMBER: %s, I_NAME: %s, SUPPLIER_WAREHOUSE: %d, QUANTITY: %d, OL_AMOUNT: %.2f, S_QUANTITY: %d",
+              itemIds.get(i), itemsName.get(i), supplierWareHouse.get(i), quantity.get(i), itemsAmount.get(i), adjustedQuantities.get(i)));
+    }
   }
 }
