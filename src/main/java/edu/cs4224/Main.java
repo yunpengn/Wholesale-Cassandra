021c8420f5c266dc19c909e0e5c35dffb83a3e30
Yunpeng Niu <neilniuyunpeng@gmail.com>
2019-09-28 10:12:21 +0800

    Print all lines instead

--- src/main/java/edu/cs4224/Main.java
+++ src/main/java/edu/cs4224/Main.java
@@ -4,16 +4,20 @@ import com.datastax.oss.driver.api.core.CqlSession;
 import com.datastax.oss.driver.api.core.cql.ResultSet;
 import com.datastax.oss.driver.api.core.cql.Row;
 
+import java.util.List;
+
 public class Main {
   public static void main(String[] args) {
-    Row row = executeQuery("select release_version from system.local");
-    System.out.println(row.getFormattedContents());
+    List<Row> result = executeQuery("select release_version from system.local");
+    for (Row row: result) {
+      System.out.println(row.getFormattedContents());
+    }
   }
 
-  private static Row executeQuery(String query) {
+  private static List<Row> executeQuery(String query) {
     try (CqlSession session = CqlSession.builder().build()) {
       ResultSet resultSet = session.execute(query);
-      return resultSet.one();
+      return resultSet.all();
     }
   }
 }
