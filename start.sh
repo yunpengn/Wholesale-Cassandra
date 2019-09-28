daa599324cc67c9a9434c3b39f0f2a8bd3551e15
hailin xiang <895227442@qq.com>
2019-09-28 13:56:27 +0800

    add start script

---
+++ start.sh
@@ -0,0 +1,37 @@
+#!/usr/bin/env bash
+
+project_path=$(dirname $(realpath $0))
+cql_path="$project_path/src/main/resources"
+data_path="$project_path/data/data-files"
+
+create_schema() {
+    cd $cql_path
+    cqlsh -f schema.cql --request-timeout=3600
+}
+
+load_data() {
+    cd $data_path
+    file="order-line.csv"
+    modified="modified-order-line.csv"
+
+    if [[ -f $modified ]]; then
+        rm $modified
+    fi
+
+    sed 's:,null,:,,:g' $file > $modified
+
+    cd $cql_path
+    cqlsh -f loaddata.cql
+
+    cd $data_path
+    rm $modified
+}
+
+
+if [[ "$1" == "createschema" ]]; then
+    create_schema
+elif [[ "$1" == "loaddata" ]]; then
+    load_data
+else
+    echo "unknown command"
+fi
