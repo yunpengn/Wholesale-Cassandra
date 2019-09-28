877c27ce3f677092bc978fde35503ab6f1e9f1e2
Yunpeng Niu <neilniuyunpeng@gmail.com>
2019-09-28 09:43:31 +0800

    Add sample query

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,17 @@
+import com.datastax.oss.driver.api.core.CqlSession;
+import com.datastax.oss.driver.api.core.cql.ResultSet;
+import com.datastax.oss.driver.api.core.cql.Row;
+
 public class Main {
   public static void main(String[] args) {
-    System.out.println("Hello world!");
+    Row row = executeQuery("select release_version from system.local");
+    System.out.println(row.getFormattedContents());
+  }
+
+  private static Row executeQuery(String query) {
+    try (CqlSession session = CqlSession.builder().build()) {
+      ResultSet resultSet = session.execute(query);
+      return resultSet.one();
+    }
   }
 }
