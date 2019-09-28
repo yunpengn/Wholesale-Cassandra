892935e7e013d5b6efebaac1425282d7bbab5aac
Yunpeng Niu <neilniuyunpeng@gmail.com>
2019-09-28 13:58:49 +0800

    Setup input framework

---
+++ src/main/java/edu/cs4224/transactions/PaymentTransaction.java
@@ -0,0 +1,11 @@
+package edu.cs4224.transactions;
+
+public class PaymentTransaction extends BaseTransaction {
+  public PaymentTransaction(final String[] parameters) {
+    super(parameters);
+  }
+
+  @Override public void execute(final String[] dataLines) {
+
+  }
+}
