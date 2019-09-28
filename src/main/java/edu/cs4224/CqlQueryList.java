2eb5d78cb6c79a8c8c47834b9c071c2f1fbd9111
junming403 <wjmfelix0403@gmail.com>
2019-09-28 17:30:55 +0800

    update

--- src/main/java/edu/cs4224/CqlQueryList.java
+++ src/main/java/edu/cs4224/CqlQueryList.java
@@ -7,7 +7,7 @@ public class CqlQueryList {
           = "UPDATE district SET D_NEXT_O_ID = %d WHERE D_W_ID = %d AND D_ID = %d";
   public static final String CREATE_NEW_ORDER
           = "INSERT INTO customer_order (O_ID, O_D_ID, O_W_ID, O_C_ID, O_ENTRY_D, O_OL_CNT, O_ALL_LOCAL) " +
-          "VALUES(%d, %d, %d, %d, %s, %d, %d)";
+          "VALUES(%d, %d, %d, %d, '%s', %d, %d)";
   public static final String CHECK_STOCK_INFO
           = "SELECT S_QUANTITY, S_REMOTE_CNT, S_ORDER_CNT, S_YTD FROM stock WHERE S_W_ID = %d AND S_I_ID = %d";
   public static final String UPDATE_STOCK
@@ -17,7 +17,7 @@ public class CqlQueryList {
           = "SELECT I_PRICE, I_NAME FROM item WHERE I_ID = %d";
   public static final String CREATE_ORDER_LINE
           = "INSERT INTO order_line(OL_O_ID, OL_D_ID, OL_W_ID, OL_NUMBER, OL_I_ID, OL_SUPPLY_W_ID, OL_QUANTITY, OL_AMOUNT, OL_DIST_INFO)" +
-          " VALUES(%d, %d, %d, %d, %d, %d, %d, %f, %s)";
+          " VALUES(%d, %d, %d, %d, %d, %d, %d, %f, '%s')";
   public static final String CHECK_WAREHOUSE_TAX
           = "SELECT W_TAX FROM warehouse WHERE W_ID=%d";
   public static final String CHECK_CUSTOMER_INFO
