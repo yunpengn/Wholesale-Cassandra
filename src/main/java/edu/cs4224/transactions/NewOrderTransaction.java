09e46b88f3dcdd667814678a3ad6b4b79d92965e
Yunpeng Niu <neilniuyunpeng@gmail.com>
2019-09-28 14:07:57 +0800

    Finish new order tx

--- src/main/java/edu/cs4224/transactions/NewOrderTransaction.java
+++ src/main/java/edu/cs4224/transactions/NewOrderTransaction.java
@@ -4,13 +4,18 @@ package edu.cs4224.transactions;
  * NewOrderTransaction is the transaction used to create a new order.
  */
 public class NewOrderTransaction extends BaseTransaction {
+  private final int customerID;
+  private final int warehouseID;
+  private final int districtID;
   private final int numDataLines;
 
   public NewOrderTransaction(final String[] parameters) {
     super(parameters);
 
-    String lastParam = parameters[parameters.length - 1];
-    numDataLines = Integer.parseInt(lastParam);
+    customerID = Integer.parseInt(parameters[1]);
+    warehouseID = Integer.parseInt(parameters[2]);
+    districtID = Integer.parseInt(parameters[3]);
+    numDataLines = Integer.parseInt(parameters[4]);
   }
 
   @Override public int numOfDataLines() {
