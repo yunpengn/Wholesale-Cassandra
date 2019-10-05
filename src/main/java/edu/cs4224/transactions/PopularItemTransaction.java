edd72270827f678c10249441b95a30d1d73ae22b
junming403 <wjmfelix0403@gmail.com>
2019-10-05 13:55:52 +0800

    update

--- src/main/java/edu/cs4224/transactions/PopularItemTransaction.java
+++ src/main/java/edu/cs4224/transactions/PopularItemTransaction.java
@@ -34,73 +34,73 @@ public class PopularItemTransaction extends BaseTransaction {
 
     @Override
     public void execute(final String[] dataLines) {
-        StringBuilder builder = new StringBuilder();
-        builder.append(String.format("1. W_ID: %d, D_ID: %d\n", W_ID, D_ID));
-        builder.append(String.format("2. L: %d\n", L));
-
-        int N = executeQuery(String.format(DISTRICT_NEXT_O_ID, W_ID, D_ID))
-                .get(0).getInt("D_NEXT_O_ID");
-
-        List<Row> S = new ArrayList<>(L);
-        for (int i = L; i > 0; i--) {
-            List<Row> orders = executeQuery(String.format(SELECT_ORDER, D_ID, W_ID, N - i));
-            if (!orders.isEmpty()) {
-                S.add(orders.get(0));
-            }
-        }
-
-        builder.append("3.\n");
-        Set<String> popularItemSet = new HashSet<>();
-        List<Set<String>> popularItemsInEveryOrder = new ArrayList<>(S.size());
-        for (Row order : S) {
-            int O_ID = order.getInt("O_ID");
-            builder.append(String.format("O_ID: %d, O_ENTRY_D: %s\n",
-                    O_ID, order.getLocalTime("O_ENTRY_D").toString()));
-
-            Row customer = executeQuery(String.format(SELECT_CUSTOMER, W_ID, D_ID, order.getInt("O_C_ID"))).get(0);
-            builder.append(String.format("C_FIRST: %s, C_MIDDLE: %s, C_LAST: %s\n",
-                    customer.getString("C_FIRST"), customer.getString("C_MIDDLE"), customer.getString("C_LAST")));
-
-            List<Row> orderLines = executeQuery(String.format(SELECT_ORDER_LINE, O_ID, D_ID, W_ID));
-            if (orderLines.isEmpty())
-                continue;
-
-            double max = orderLines.get(0).getBigDecimal("OL_QUANTITY").doubleValue();
-            for (int i = 1; i < orderLines.size(); i++) {
-                double quantity = orderLines.get(i).getBigDecimal("OL_QUANTITY").doubleValue();
-                if (max < quantity) {
-                    max = quantity;
-                }
-            }
-
-            Set<String> items = new HashSet<>();
-            for (Row orderLine: orderLines) {
-                double quantity = orderLine.getDouble("OL_QUANTITY");
-                if (quantity == max) {
-                    Row item = executeQuery(String.format(SELECT_ITEM, orderLine.getInt("OL_I_ID"))).get(0);
-
-                    String itemName = item.getString("I_NAME");
-                    popularItemSet.add(itemName);
-                    items.add(itemName);
-
-                    builder.append(String.format("I_NAME: %s, OL_QUANTITY: %.1f\n", itemName, quantity));
-                }
-            }
-            popularItemsInEveryOrder.add(items);
-            builder.append("\n");
-        }
-
-        builder.append("4.\n");
-        for (String itemName: popularItemSet) {
-            int count = 0;
-            for (Set<String> items: popularItemsInEveryOrder) {
-                if (items.contains(itemName)) {
-                    count++;
-                }
-            }
-            builder.append(String.format("I_NAME: %s, percentage: %2.2f%%\n", itemName, count * 100.0 / popularItemSet.size()));
-        }
-
-        System.out.println(builder.toString());
+//        StringBuilder builder = new StringBuilder();
+//        builder.append(String.format("1. W_ID: %d, D_ID: %d\n", W_ID, D_ID));
+//        builder.append(String.format("2. L: %d\n", L));
+//
+//        int N = executeQuery(String.format(DISTRICT_NEXT_O_ID, W_ID, D_ID))
+//                .get(0).getInt("D_NEXT_O_ID");
+//
+//        List<Row> S = new ArrayList<>(L);
+//        for (int i = L; i > 0; i--) {
+//            List<Row> orders = executeQuery(String.format(SELECT_ORDER, D_ID, W_ID, N - i));
+//            if (!orders.isEmpty()) {
+//                S.add(orders.get(0));
+//            }
+//        }
+//
+//        builder.append("3.\n");
+//        Set<String> popularItemSet = new HashSet<>();
+//        List<Set<String>> popularItemsInEveryOrder = new ArrayList<>(S.size());
+//        for (Row order : S) {
+//            int O_ID = order.getInt("O_ID");
+//            builder.append(String.format("O_ID: %d, O_ENTRY_D: %s\n",
+//                    O_ID, order.getLocalTime("O_ENTRY_D").toString()));
+//
+//            Row customer = executeQuery(String.format(SELECT_CUSTOMER, W_ID, D_ID, order.getInt("O_C_ID"))).get(0);
+//            builder.append(String.format("C_FIRST: %s, C_MIDDLE: %s, C_LAST: %s\n",
+//                    customer.getString("C_FIRST"), customer.getString("C_MIDDLE"), customer.getString("C_LAST")));
+//
+//            List<Row> orderLines = executeQuery(String.format(SELECT_ORDER_LINE, O_ID, D_ID, W_ID));
+//            if (orderLines.isEmpty())
+//                continue;
+//
+//            double max = orderLines.get(0).getBigDecimal("OL_QUANTITY").doubleValue();
+//            for (int i = 1; i < orderLines.size(); i++) {
+//                double quantity = orderLines.get(i).getBigDecimal("OL_QUANTITY").doubleValue();
+//                if (max < quantity) {
+//                    max = quantity;
+//                }
+//            }
+//
+//            Set<String> items = new HashSet<>();
+//            for (Row orderLine: orderLines) {
+//                double quantity = orderLine.getDouble("OL_QUANTITY");
+//                if (quantity == max) {
+//                    Row item = executeQuery(String.format(SELECT_ITEM, orderLine.getInt("OL_I_ID"))).get(0);
+//
+//                    String itemName = item.getString("I_NAME");
+//                    popularItemSet.add(itemName);
+//                    items.add(itemName);
+//
+//                    builder.append(String.format("I_NAME: %s, OL_QUANTITY: %.1f\n", itemName, quantity));
+//                }
+//            }
+//            popularItemsInEveryOrder.add(items);
+//            builder.append("\n");
+//        }
+//
+//        builder.append("4.\n");
+//        for (String itemName: popularItemSet) {
+//            int count = 0;
+//            for (Set<String> items: popularItemsInEveryOrder) {
+//                if (items.contains(itemName)) {
+//                    count++;
+//                }
+//            }
+//            builder.append(String.format("I_NAME: %s, percentage: %2.2f%%\n", itemName, count * 100.0 / popularItemSet.size()));
+//        }
+//
+//        System.out.println(builder.toString());
     }
 }
--- src/main/java/edu/cs4224/transactions/TopBalanceTransaction.java
+++ src/main/java/edu/cs4224/transactions/TopBalanceTransaction.java
@@ -23,25 +23,25 @@ public class TopBalanceTransaction extends BaseTransaction {
         // TODO need to add C_BALANCE into clustering key
         // find top 10 customers with maximum balance
 
-        List<Row> rows = executeQuery(SELECT_CUSTMER_ORDER_BY);
-        Collections.sort(rows, (o1, o2) -> {
-            double b1 = o1.getBigDecimal("C_BALANCE").doubleValue();
-            double b2 = o2.getBigDecimal("C_BALANCE").doubleValue();
-
-            return -1 * Double.compare(b1, b2);
-        });
-
-        for (int i = 0; i < 10 && rows.size() > i; i++) {
-            Row customer = rows.get(i);
-            String warehouseName = executeQuery(String.format(SELECT_WAREHOUSE_NAME_BY_ID,
-                    customer.getInt("C_W_ID"))).get(0).getString("W_NAME");
-            String districtName = executeQuery(String.format(SELECT_DISTRICT_NAME_BY_ID,
-                    customer.getInt("C_W_ID"), customer.getInt("C_D_ID"))).get(0).getString("D_NAME");
-
-            System.out.printf("C_FIRST: %s, C_MIDDLE: %s, C_LAST: %s, C_BALANCE: %.1f, W_NAME: %s, D_NAME: %s\n",
-                    customer.getString("C_FIRST"), customer.getString("C_MIDDLE"), customer.getString("C_LAST"),
-                    customer.getBigDecimal("C_BALANCE").doubleValue(), warehouseName, districtName);
-        }
+//        List<Row> rows = executeQuery(SELECT_CUSTMER_ORDER_BY);
+//        Collections.sort(rows, (o1, o2) -> {
+//            double b1 = o1.getBigDecimal("C_BALANCE").doubleValue();
+//            double b2 = o2.getBigDecimal("C_BALANCE").doubleValue();
+//
+//            return -1 * Double.compare(b1, b2);
+//        });
+//
+//        for (int i = 0; i < 10 && rows.size() > i; i++) {
+//            Row customer = rows.get(i);
+//            String warehouseName = executeQuery(String.format(SELECT_WAREHOUSE_NAME_BY_ID,
+//                    customer.getInt("C_W_ID"))).get(0).getString("W_NAME");
+//            String districtName = executeQuery(String.format(SELECT_DISTRICT_NAME_BY_ID,
+//                    customer.getInt("C_W_ID"), customer.getInt("C_D_ID"))).get(0).getString("D_NAME");
+//
+//            System.out.printf("C_FIRST: %s, C_MIDDLE: %s, C_LAST: %s, C_BALANCE: %.1f, W_NAME: %s, D_NAME: %s\n",
+//                    customer.getString("C_FIRST"), customer.getString("C_MIDDLE"), customer.getString("C_LAST"),
+//                    customer.getBigDecimal("C_BALANCE").doubleValue(), warehouseName, districtName);
+//        }
 
 
         // Way 2:
