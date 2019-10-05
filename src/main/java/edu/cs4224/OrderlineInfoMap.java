8d77b0bcf543788ce7927c199ea7f63f61943825
junming403 <wjmfelix0403@gmail.com>
2019-10-05 14:36:47 +0800

    update

--- src/main/java/edu/cs4224/OrderlineInfoMap.java
+++ src/main/java/edu/cs4224/OrderlineInfoMap.java
@@ -1,6 +1,7 @@
 package edu.cs4224;
 
 import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.TypeReference;
 import com.datastax.oss.driver.api.core.cql.Row;
 
 import java.util.HashMap;
@@ -22,6 +23,6 @@ public class OrderlineInfoMap extends HashMap<Integer, OrderlineInfo> {
     }
 
     public static OrderlineInfoMap fromJson(String json) {
-        return JSON.parseObject(json, OrderlineInfoMap.class);
+        return (OrderlineInfoMap) JSON.parseObject(json, new TypeReference<HashMap<Integer, OrderlineInfo>>() {});
     }
 }
