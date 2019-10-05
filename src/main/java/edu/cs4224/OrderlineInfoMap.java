c9f56ea9ba2e9cc325736914b0237627ceab71ad
Yunpeng Niu <neilniuyunpeng@gmail.com>
2019-10-05 14:47:03 +0800

    Use Gson rather than fastjsonc

--- src/main/java/edu/cs4224/OrderlineInfoMap.java
+++ src/main/java/edu/cs4224/OrderlineInfoMap.java
@@ -1,13 +1,15 @@
 package edu.cs4224;
 
-import com.alibaba.fastjson.JSON;
-import com.alibaba.fastjson.TypeReference;
 import com.datastax.oss.driver.api.core.cql.Row;
+import com.google.gson.Gson;
 
 import java.util.HashMap;
 import java.util.List;
 
 public class OrderlineInfoMap extends HashMap<Integer, OrderlineInfo> {
+    public static void main(String[] args) {
+        System.out.println("123");
+    }
 
     public OrderlineInfoMap() {
     }
@@ -19,10 +21,10 @@ public class OrderlineInfoMap extends HashMap<Integer, OrderlineInfo> {
     }
 
     public String toJson() {
-        return JSON.toJSONString(this);
+        return new Gson().toJson(this);
     }
 
     public static OrderlineInfoMap fromJson(String json) {
-        return JSON.parseObject(json, new TypeReference<OrderlineInfoMap>() {});
+        return new Gson().fromJson(json, OrderlineInfoMap.class);
     }
 }
